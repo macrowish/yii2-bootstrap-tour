fc2cef835ed0fc8057ceae62d0c4f3068cac92ef
Dean <contact@macrowish.com>
2016-08-19 16:59:15 +0200

    Init commit

---
+++ BootstrapTour.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace macrowish\widgets;
+
+/**
+ * This is just an example.
+ */
+class BootstrapTour extends \yii\base\Widget
+{
+    public $options = [];
+    public $steps = [];
+
+    public function run()
+    {
+
+        $view = $this->getView();
+        $view->registerCssFile('//cdnjs.cloudflare.com/ajax/libs/bootstrap-tour/0.11.0/css/bootstrap-tour.min.css');
+        $view->registerJsFile('//cdnjs.cloudflare.com/ajax/libs/bootstrap-tour/0.11.0/js/bootstrap-tour.min.js', ['depends' => [\yii\bootstrap\BootstrapPluginAsset::className()]]);
+
+        $encodedSteps = json_encode($this->steps);
+        $encodedOptions = json_encode($this->options);
+
+        $view->registerJs(<<<JS
+// Instance the tour
+var tour = new Tour({
+  $encodedOptions;
+  steps: [
+  $encodedSteps
+]});
+
+// Initialize the tour
+tour.init();
+
+// Start the tour
+tour.start();
+JS
+        );
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+Yii2 Bootstrap Tour
+===================
+Bootstrap Tour widget for Yii2
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist macrowish/yii2-bootstrap-tour "*"
+```
+
+or add
+
+```
+"macrowish/yii2-bootstrap-tour": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \macrowish\widgets\AutoloadExample::widget(); ?>```
